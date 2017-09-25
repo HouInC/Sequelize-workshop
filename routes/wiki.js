334e0252bb1427fc4b0ef687be5a2d76414165d5
Hou In Choi <hichoi17@outlook.com>
2017-09-25 15:04:22 -0400

    set author ok

--- routes/wiki.js
+++ routes/wiki.js
@@ -18,24 +18,31 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/', function(req, res, next){
-
   User.findOrCreate({
-    where:{
-    name : req.body.full_name,
-    email: req.body.email
-  }}).then(function(values){
+    where: {
+      name: req.body.full_name,
+      email: req.body.email
+    }
+  })
+  .then(function (values) {
+
     var user = values[0];
+
     var page = Page.build({
-      title : req.body.title,
-      content:req.body.content
+      title: req.body.title,
+      content: req.body.content
     });
 
-    return page.save().then(page=> {
-      console.log(page);
+    return page.save().then(function (page) {
+      return page.setAuthor(user); // the set method don't need to define, it
     });
-  }).then(page=>res.redirect(page.getRoute)
-  ).catch(next);
-})
+
+  })
+  .then(function (page){
+    res.redirect(page.getRoute);
+  })
+  .catch(next);
+  });
 
 router.get('/add', function(req, res, next){
   res.render('addpage')
@@ -55,7 +62,6 @@ router.get('/:urlTitle',function(req,res,next){
     }else{
       res.redirect('/');
     }
-    
   }).catch(next);
   //res.json(page);
   // res.send('hit dynamic route at '+req.params.urlTitle);
